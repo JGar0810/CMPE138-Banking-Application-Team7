0772eea3deb5b85b7624ec4c32b2859d6bd7f9c1
lukelamle <106286689+lukelamle@users.noreply.github.com>
2026-05-05 00:10:47 -0700

    Enhance SQL queries with limits and new selections
    Added new queries to limit results and retrieve recent loans.

--- SQL/luke_test_queries.sql
+++ SQL/luke_test_queries.sql
@@ -1,16 +1,20 @@
 USE banking_system;
-SHOW TABLES;
 
-SELECT * FROM vw_customer_accounts;
-SELECT * FROM vw_account_transactions;
-SELECT * FROM vw_customer_loans;
-
-CALL sp_get_customer_accounts(1);
-CALL sp_get_account_transactions(1);
-CALL sp_get_customer_summary(1);
+SHOW TRIGGERS;
 
 SHOW INDEX FROM Account;
 SHOW INDEX FROM `Transaction`;
 SHOW INDEX FROM Loan;
 
-SHOW TRIGGERS;
+SELECT * FROM vw_customer_accounts LIMIT 5;
+SELECT * FROM vw_account_transactions LIMIT 5;
+SELECT * FROM vw_customer_loans LIMIT 5;
+
+CALL sp_get_customer_accounts(1);
+CALL sp_get_account_transactions(1);
+CALL sp_get_customer_summary(1);
+
+SELECT loan_id, customer_id, loan_amount, status
+FROM Loan
+ORDER BY loan_id DESC
+LIMIT 5;
